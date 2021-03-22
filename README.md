c346f4eb43ed37c0290a9c5ce1d1423336c39467
Loisel <loiselloiselus@gmail.com>
2021-03-22 12:08:00 +0100

    added comments

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Todos:
   1. automatische Rechteverwaltung
   2. kalender
   3. musik
-  4. ranking
+  4. ranking / level system - elias
   5. embedded messages
--- src/main/main.js
+++ src/main/main.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
-const prefix = process.env.PREFIX;
-
 const { Client } = require('discord.js');
+
+// discord client
 const client = new Client();
 
+// Emitted when the client becomes ready to start working
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in!`);
 });
 
+// login with token
 client.login(process.env.BOT_TOKEN);
